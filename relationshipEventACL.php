49e2761352c74e583b7b5b51669533ca37d8ed3b
Antti Kekki <antti.kekki@gmail.com>
2014-01-12 22:41:21 +0200

    Moved code to directory

--- relationshipEventACL.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once "RelationshipEventACLWorker.php";
-
-function relationshipEventACL_civicrm_pageRun(&$page) {
-  $worker = new RelationshipEventACLWorker();
-  $worker->run($page);
-}
