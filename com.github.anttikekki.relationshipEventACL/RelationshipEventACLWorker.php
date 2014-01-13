d5859ae16a192648d5598a3e7f937f3e01541b00
Antti Kekki <antti.kekki@gmail.com>
2014-01-13 18:11:41 +0200

    Event dashboard is now filtered

--- com.github.anttikekki.relationshipEventACL/RelationshipEventACLWorker.php
+++ com.github.anttikekki.relationshipEventACL/RelationshipEventACLWorker.php
@@ -12,76 +12,123 @@ if(class_exists('RelationshipACLQueryWorker') === false) {
  * Worker to solve Event visibility and edit rights for user from relationship edit rights.
  */
 class RelationshipEventACLWorker {
+  /**
+  * Event custom field table name.
+  */
   private $eventCustomFieldTable = "civicrm_value_tapahtuman_omistajuus_2";
+  
+  /**
+  * Event custom field table contact ID column name.
+  */
   private $eventCustomFieldContactIDColumn = "j_rjest_j_organisaatio_1";
   
   /**
   * Start worker.
   *
-  * @param Page|Form $page CiviCRM Page or Form object from hook
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object from hook
   */
   public function run(&$page) {
     //Manage events
     if($page instanceof CRM_Event_Page_ManageEvent) {
-      $this->filterEventRows($page);
+      $this->filterManagementEventRows($page);
       $this->createPager($page);
     }
     //Edit event
     else if($page instanceof CRM_Event_Form_ManageEvent) {
       $this->checkEventEditPermission($page);
     }
+    //Dashboard
+    else if($page instanceof CRM_Event_Page_DashBoard) {
+      $this->filterDashBoardEventRows($page);
+    }
   }
   
   /**
   * Check if current logged in user has rights to edit selected event. Show fatal error if no permission.
   *
-  * @param Page|Form $page CiviCRM Page or Form object
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object
   */
   private function checkEventEditPermission(&$page) {
     $eventID = $page->_id;
-     
-    $currentUserContactID = $this->getCurrentUserContactID();
-    $allowedContactIDs = $this->getContactIDsWithEditPermissions($currentUserContactID);
     
-    //Array with event ID as key and event owner contact ID as value
-    $eventOwnerMap = $this->getEventOwnerCustomFieldContactIDs();
+    $rows = array();
+    $rows[$eventID] = array();
+    $this->filterEventRows($rows);
     
-    if(!array_key_exists($eventID, $eventOwnerMap)) {
-      return;
-    }
-    
-    $eventOwnerContactID = $eventOwnerMap[$eventID];
-    
-    if(!in_array($eventOwnerContactID, $allowedContactIDs)) {
+    if(count($rows) === 0) {
       CRM_Core_Error::fatal(ts('You do not have permission to view this event'));
     }
   }
   
-  private function filterEventRows(&$page) {
+  /**
+  * Iterates 'row' array from template and removes events where current logged in user does not have 
+  * editing rights. Editing rights are based on relationship tree.
+  *
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object
+  */
+  private function filterManagementEventRows(&$page) {
     $template = $page->getTemplate();
     $rows = $template->get_template_vars("rows");
   
+    $this->filterEventRows($rows);
+    
+    $page->assign("rows", $rows);
+  }
+  
+  /**
+  * Iterates 'eventSummary' variable 'events' array from template and removes events where current logged in user does not have 
+  * editing rights. Editing rights are based on relationship tree.
+  *
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object
+  */
+  private function filterDashBoardEventRows(&$page) {
+    $template = $page->getTemplate();
+    $eventSummary = $template->get_template_vars("eventSummary");
+    $rows = $eventSummary["events"];
+  
+    $this->filterEventRows($rows);
+    
+    $eventSummary["events"] = $rows;
+    $page->assign("eventSummary", $eventSummary);
+  }
+  
+  /**
+  * Iterates rows array from template and removes events where current logged in user does not have 
+  * editing rights. Editing rights are based on relationship tree.
+  *
+  * @param array $rows Array of events
+  */
+  private function filterEventRows(&$rows) {
     $currentUserContactID = $this->getCurrentUserContactID();
+    
+    //All contact IDs the current logged in user has rights to edit through relationships
     $allowedContactIDs = $this->getContactIDsWithEditPermissions($currentUserContactID);
     
     //Array with event ID as key and event owner contact ID as value
     $eventOwnerMap = $this->getEventOwnerCustomFieldContactIDs();
     
     foreach ($rows as $eventID => &$row) {
+      //Skip events that does not have owner info. These are always visible.
       if(!array_key_exists($eventID, $eventOwnerMap)) {
         continue;
       }
       
+      //Get event owner contact ID from custom field
       $eventOwnerContactID = $eventOwnerMap[$eventID];
       
+      //If logged in user contact ID is not allowed to edit event, remove event from array
       if(!in_array($eventOwnerContactID, $allowedContactIDs)) {
         unset($rows[$eventID]);
       }
     }
-    
-    $page->assign("rows", $rows);
   }
   
+  /**
+  * Count the length of 'rows' array from template. Counts only rows with integer id.
+  *
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object
+  * @return int rowcount
+  */
   private function getEventRowCount(&$page) {
     $template = $page->getTemplate();
     $rows = $template->get_template_vars("rows");
@@ -96,6 +143,11 @@ class RelationshipEventACLWorker {
     return $eventCount;
   }
   
+  /**
+  * Recreates pager to update it to new rowcount after filtering.
+  *
+  * @param CRM_Core_Page|CRM_Core_Form $page CiviCRM Page or Form object
+  */
   private function createPager(&$page) {
     $rowCount = $this->getEventRowCount($page);
   
@@ -113,6 +165,11 @@ class RelationshipEventACLWorker {
     $page->assign_by_ref('pager', $pager);
   }
   
+  /**
+  * Returns current logged in user contact ID.
+  *
+  * @return int Contact ID
+  */
   private function getCurrentUserContactID() {
     global $user;
     $userID = $user->uid;
@@ -128,6 +185,11 @@ class RelationshipEventACLWorker {
     return $contact_id;
   }
 
+  /**
+  * Loads all event owner custom field data.
+  *
+  * @return array Associative array where key is event ID and value is contact ID.
+  */
   private function getEventOwnerCustomFieldContactIDs() {
     $contactIDColumn = $this->eventCustomFieldContactIDColumn;
     $customFieldTable = $this->eventCustomFieldTable;
@@ -145,6 +207,12 @@ class RelationshipEventACLWorker {
     return $result;
   }
 
+  /**
+  * Loads all contact IDs where current logged in user has edit rights trouhg relationships.
+  * Uses RelationshipACLQueryWorker.
+  *
+  * @return int[] Contact IDs.
+  */
   private function getContactIDsWithEditPermissions($contactID) {
     $worker = new RelationshipACLQueryWorker();
     $contactTableName = $worker->createContactsTableWithEditPermissions($contactID);
--- com.github.anttikekki.relationshipEventACL/relationshipEventACL.php
+++ com.github.anttikekki.relationshipEventACL/relationshipEventACL.php
@@ -2,11 +2,22 @@
 
 require_once "RelationshipEventACLWorker.php";
 
+/**
+* Implemets CiviCRM 'pageRun' hook.
+*
+* @param CRM_Core_Page $page Current page.
+*/
 function relationshipEventACL_civicrm_pageRun(&$page) {
   $worker = new RelationshipEventACLWorker();
   $worker->run($page);
 }
 
+/**
+* Implemets CiviCRM 'buildForm' hook.
+*
+* @param String $formName Name of current form.
+* @param CRM_Core_Form $form Current form.
+*/
 function relationshipEventACL_civicrm_buildForm($formName, &$form) {
   $worker = new RelationshipEventACLWorker();
   $worker->run($form);
